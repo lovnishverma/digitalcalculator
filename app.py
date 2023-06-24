0b53c06eaefdc40bbf5e085c26fbf9d98d81cf61
Glitch (glitch-hello-website) <none>
2023-06-24 16:53:08 +0000

    🍭💎 Checkpoint ./app.py:59316741/501 ./templates/calc.html:59316741/45 ./requirements.txt:59316741/2

--- app.py
+++ app.py
@@ -3,23 +3,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-#number1=eval(input("Enter first number: "))
-
 @app.route('/')
 def index():
     return render_template('index.html')
   
 @app.route('/calc')
 def calculator():
-  return render_template('calc.html')
-num1= int(request.form['num1'])
-num2= int(request.form['num2'])
-add= int(request.form['add'])
-sub= int(request.form['sub'])
-mul= int(request.form['mul'])
-div= int(request.form['div'])
-result = num1
-
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+    #add= int(request.form['add'])
+    # sub= int(request.form['sub'])
+    # mul= int(request.form['mul'])
+    # div= int(request.form['div'])
+    result = num1 + num2
+    return render_template('calc.html', result)
     
 if __name__ =='__main__':
   app.run(debug=True)
