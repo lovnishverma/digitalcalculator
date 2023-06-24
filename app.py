092e74ed912f6c46fae3f8a78cce9fc47e1511ae
Glitch (glitch-hello-website) <none>
2023-06-24 17:04:32 +0000

    💶🐇 Checkpoint ./app.py:59316741/205 ./templates/calc.html:59316741/22

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
   
-@app.route('/calc')
-def calculator():
+@app.route('/calc', methods=['POST'])
+def calc():
+  if request.method == 'POST':
     num1 = int(request.form['num1'])
     num2 = int(request.form['num2'])
     #add= int(request.form['add'])
     # sub= int(request.form['sub'])
     # mul= int(request.form['mul'])
     # div= int(request.form['div'])
-    result = num1 + num2
+    result = ([[num1 + num2]])
     return render_template('calc.html', result)
     
 if __name__ =='__main__':
