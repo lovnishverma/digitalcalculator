6dbe71e26561ac4cf32853c0acf0ae7bed331501
Glitch (glitch-hello-website) <none>
2023-06-24 16:19:28 +0000

    🏕🎹 Checkpoint ./requirements.txt:59316741/9 ./index.html:59316741/222 ./app.py:59316741/500 ./templates/index.html:59316741/2230

--- app.py
+++ app.py
@@ -4,32 +4,27 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #number1=eval(input("Enter first number: "))
+
 @app.route('/')
 def index():
     return render_template('index.html')
   
-
-
 @app.route('/calc')
-def calculator(number):
-    sign=str(input("Enter sign: "))
-    number2=eval(input("Enter another number: "))
-    if sign=='+':
-        number=number+number2
-        print(number)
-    elif sign=='-':
-        number=number-number2
-        print(number)
-    elif sign=='*':
-        number=number*number2
-        print(number)
-    elif sign=='/':
-        number=number/number2
-        print(number)
-    a=int(input("enter 0 for continue and 1 for quit: "))
-    if a==0:
-        calculator(number)
-
-# if b==1:
-#     calculator(sign)
-a=calculator(number1)
+def calculator():
+  return render_template('calc.html')
+#     sign=str(input("Enter sign: "))
+#     number2=eval(input("Enter another number: "))
+#     if sign=='+':
+#         number=number+number2
+#         print(number)
+#     elif sign=='-':
+#         number=number-number2
+#         print(number)
+#     elif sign=='*':
+#         number=number*number2
+#         print(number)
+#     elif sign=='/':
+#         number=number/number2
+#         print(number)
+if __name__ =='__main__':
+  app.run(debug=True)
