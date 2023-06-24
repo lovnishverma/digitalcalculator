6901c1d71fb2014cae333dbb9052ea3d05d40517
Glitch (glitch-hello-website) <none>
2023-06-24 18:00:00 +0000

    👨‍❤️‍👨🎋 Checkpoint ./app.py:57629255/3898 ./templates/calc.html:57629255/11539 ./templates/index.html:57629255/5

--- app.py
+++ app.py
@@ -6,33 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/calc', methods=['GET','POST'])
-def calc():
-    if request.method == 'POST':
-        try:
-            num1 = int(request.form['num1'])
-            num2 = int(request.form['num2'])
-            operator = request.form['operator']
-            
-            if operator == '+':
-                result = num1 + num2
-            elif operator == '-':
-                result = num1 - num2
-            elif operator == '*':
-                result = num1 * num2
-            elif operator == '/':
-                result = num1 / num2
-            else:
-                error_message = "Invalid operator selected."
-                return render_template('calc.html', error=error_message)
-                
-            return render_template('calc.html', result= result)
-        
-        except (KeyError, ValueError):
-            error_message = "Invalid input. Please enter valid numbers."
-            return render_template('calc.html', error=error_message)
-    
-    return render_template('calc.html')
+@app.route('/calculate', methods=['GET','POST'])
+def calculate():
+    try:
+        expression = request.form['expression']
+        result = eval(expression)
+        return render_template('calc.html', expression=expression, result=result)
+    except Exception as e:
+        error_message = str(e)
+        return render_template('calc.html', error=error_message)
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
