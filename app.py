41ea531fad59ab918c37eabfe57c390b0aa0ca2a
Glitch (glitch-hello-website) <none>
2023-06-24 17:26:59 +0000

    🦁🍓 Checkpoint ./templates/calc.html:59316741/1525 ./app.py:59316741/1966 ./requirements.txt:59316741/18

--- app.py
+++ app.py
@@ -12,11 +12,26 @@ def calc():
         try:
             num1 = int(request.form['num1'])
             num2 = int(request.form['num2'])
-            result = num1 + num2
-            return render_template('calc.html', result=result)
+            operator = request.form['operator']
+            
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                result = num1 / num2
+            else:
+                error_message = "Invalid operator selected."
+                return render_template('calc.html', error=error_message)
+                
+            return render_template('calc.html', result= 'result')
+        
         except (KeyError, ValueError):
             error_message = "Invalid input. Please enter valid numbers."
             return render_template('calc.html', error=error_message)
+    
     return render_template('calc.html')
 
 if __name__ =='__main__':
